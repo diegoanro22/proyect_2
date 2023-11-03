aadd37268dd8a30b131f17cb3118466f547c851c
diegoanro22 <diegorosalesv22@gmail.com>
2023-11-03 08:33:49 -0600

    Create Driver.py
    DRIVER

---
+++ Driver.py
@@ -0,0 +1,19 @@
+import Usuario
+
+
+class driver():
+    def __init__(self):
+        pass
+    
+    def registrarDatos(self):
+        nombre = str(self.entryEdad.get())
+        apellido = str(self.entryApellido.get())
+        edad = int(self.entryEdad.get())
+        nacionalidad = str(self.entryNacionalidad.get())
+        correo = str(self.entryCorreo.get())
+        usuario = str(self.entryNuevoUsuario.get())
+        contraseña = str(self.entryNuevoContra.get())
+        
+        newUsuario = Usuario.Usuario(nombre,apellido,edad,nacionalidad,correo,usuario,contraseña)
+        print(newUsuario)
+        print(newUsuario.getNombre())
